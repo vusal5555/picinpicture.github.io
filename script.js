d1eb634ed3c576bcced6b7c130cf75d8651fa5a4
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2023-03-01 10:16:21 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const videElement = document.getElementById('video');
+const btn = document.getElementById('button');
+
+//Prompt use to select media stream, pass to video element, then play
+
+const selectMedia = async function () {
+  try {
+    const media = await navigator.mediaDevices.getDisplayMedia();
+
+    videElement.srcObject = media;
+
+    videElement.onloadedmetadata = () => {
+      videElement.play();
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+btn.addEventListener('click', async () => {
+  btn.disabled = true;
+
+  await videElement.requestPictureInPicture();
+
+  btn.disabled = false;
+});
+
+selectMedia();
